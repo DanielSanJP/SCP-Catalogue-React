ec93e2e5f1dccedf9987ceba5c6101d1617553d9
DanielSanJP <105352895+DanielSanJP@users.noreply.github.com>
2025-03-26 11:08:07 +1300

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ const isNetlify = !!process.env.URL;
 const basePath = isNetlify ? "/" : "/SCP-Catalogue-React/";
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/SCP-Catalogue-React/", // MUST match your GitHub repo name
-=======
   base: basePath,
->>>>>>> 4a813addb76a9e0b1d2582e5a13a6e19337b4800
   plugins: [react()],
 });
