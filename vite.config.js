2aa1035bd00d5d37d31d92af7e784699b7362bd2
DanielSanJP <105352895+DanielSanJP@users.noreply.github.com>
2025-03-26 10:58:59 +1300

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/SCP-Catalogue-React/",
+  base: "/SCP-Catalogue-React/", // MUST match your GitHub repo name
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
 });
